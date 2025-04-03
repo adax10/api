416ea0214fe0de9531c02964e28f286fc3474ede
Ada Krupa <74727994+adax10@users.noreply.github.com>
2025-04-03 11:56:52 +0200

    chore: update ramda utils

--- src/lib/utils/ramda.ts
+++ src/lib/utils/ramda.ts
@@ -1,9 +1,24 @@
-import {
+import { toPairs, fromPairs, isNotNil } from 'ramda'
+
+export const hasElements = <T>(subject: T) => (Array.isArray(subject) ? subject.length > 0 : false)
+export const hasKeys = <T>(subject: T) => (typeof subject === 'object' && subject !== null ? Object.keys(subject as object).length > 0 : false)
+export const isDefined = <T>(subject: T): subject is NonNullable<T> => typeof subject !== 'undefined' && subject !== null
+
+// eslint-disable-next-line functional/functional-parameters
+export const all = (...args: Array<boolean>) => !args.some(arg => !arg)
+export const clearObject = <T>(subject: Record<string, T>): Record<string, T> => {
+    const filteredArray = toPairs(subject as { [key: string]: T }).filter(([, value]) => isNotNil(value) && value !== '')
+
+    return fromPairs(filteredArray)
+}
+
+export {
     values,
     toPairs,
     fromPairs,
     compose,
     isNil,
+    isNotNil,
     is,
     isEmpty,
     cond,
@@ -22,46 +37,3 @@ import {
     splitEvery,
     last,
 } from 'ramda'
-
-const hasElements = <T>(subject: T) => (Array.isArray(subject) ? subject.length > 0 : false)
-const hasKeys = <T>(subject: T) => (typeof subject === 'object' && subject !== null ? Object.keys(subject as object).length > 0 : false)
-const isDefined = <T>(subject: T): subject is NonNullable<T> => typeof subject !== 'undefined' && subject !== null
-const notNil = <T>(subject: T) => !isNil(subject)
-// eslint-disable-next-line functional/functional-parameters
-const all = (...args: Array<boolean>) => !args.some(arg => !arg)
-const clearObject = <T>(subject: Record<string, T>): Record<string, T> => {
-    const filteredArray = toPairs(subject as { [key: string]: T }).filter(([, value]) => notNil(value) && value !== '')
-
-    return fromPairs(filteredArray)
-}
-
-export {
-    all,
-    values,
-    hasKeys,
-    clearObject,
-    toPairs,
-    notNil,
-    isNil,
-    is,
-    hasElements,
-    isEmpty,
-    cond,
-    equals,
-    compose,
-    isDefined,
-    flatten,
-    empty,
-    always,
-    uniq,
-    T,
-    prop,
-    sortBy,
-    reverse,
-    descend,
-    sort,
-    fromPairs,
-    splitEvery,
-    sum,
-    last,
-}
